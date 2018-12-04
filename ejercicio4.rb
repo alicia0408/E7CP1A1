7892daabe7e1290ef12f3294e8de31cc8b3c453e
Alicia <tucorreo@mail.com>
2018-12-03 21:13:13 -0300

    resolucion ejercicio 4

---
+++ ejercicio4.rb
@@ -0,0 +1,9 @@
+#Array y Hashes
+
+#Se tienen dos arrays uno con el nombre de personas y otro con las edades, se pide generar un hash con el nombre y edad 
+#de cada persona (se asume que no existen dos personas con el mismo nombre)
+
+personas = ["Carolina", "Alejandro", "Maria Jesús", "Valentín"]
+edades = [32, 28, 41, 19]
+new_hash = [personas,edades].transpose.to_h
+puts new_hash
